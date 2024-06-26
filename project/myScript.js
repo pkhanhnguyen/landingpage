5fdc287a546348f1add68bdf1332f27e87b9ecf7
seebm-code <manh.nguyen@vnresource.org>
2024-06-26 15:42:32 +0700

    first commit

---
+++ project/myScript.js
@@ -0,0 +1,55 @@
+/*document.getElementById(id)	 
+document.getElementsByTagName(name)	
+document.getElementsByClassName(name)	
+document.querySelectorAll("p.intro");
+document.createElement(element) => tạo element
+document.appendChild(element) => set html/children
+
+element.value =>  với thẻ/element input 
+element.innerHTML ; => Lấy giá trị HTML hoặc gán giá trị HTML
+element.innerText ; => Lấy giá trị text hoặc gán giá trị text
+
+element.attributes => thuộc tính của element
+element.setAttribute => gán thuộc tính của element
+
+
+Object => cú pháp khao báo {}, hoặc string, int...
+Array => cú pháp khao báo  là trong ngoặc vuông [], bên trong [] là object
+Function =>cú pháp khao báo  function Ten_function () {}
+
+Array Methods
+ Array pop() => xóa 1 phấn tử cuối của mảng
+ Array push() => thêm 1 phấn tử vảo cuối mảng
+ Array shift() => xóa 1 phấn tử đầu của mảng
+ Array unshift() => thêm 1 phấn tử vảo đầu mảng
+ Array includes() => kiểm tra phần tử tổn tại trong mảng (string/int ...)
+ Array every() => kiểm tra phần tử tất cả phần trong mảng object
+ Array some()  => kiểm tra phần tử tổn tại trong mảng object
+Array forEach()  => duyệt qua tất cả phần tử trong mảng
+if, else, and else if
+Switch 
+The For In Loop
+While 
+JSON => JSON.stringify => chuyển đổi dữ liệu từ object qua string json
+JSON => JSON.parse => chuyển đổi dữ liệu từ string qua object json
+JavaScript Events
+    onchange => dùng cho (input/select)
+    onclick	=> dủng cho button
+    onmouseover	=> dùng khi di chuyễn chuột vảo element
+    onmouseout	=> dùng khi di chuyễn chuột ra khỏi element
+    onkeydown	=> dùng khi gõ bàn phím (nhập dữ liệu cho ô input)
+    onload	=> dủng khi load trang
+
+
+
+responsive topnav
+hover tabs
+split navigation
+search bar
+hover dropdown!!!
+dropdown in topnav
+
+*/
+
+
+document.getElementById("").setAttribute
